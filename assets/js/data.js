bfde89780afa26991f416c79071c378ea66d70c9
Alejandro Valles <alevata31@gmail.com>
2025-09-24 16:15:06 +0200

    mi alma se perfuma si te ve

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,122 +1,122 @@
 export const memories = [
 	{
-		image: "./assets/img/087BC221-0CE5-4071-B084-BBBA13C0F408.jpeg",
+		image: "/gordi/assets/img/087BC221-0CE5-4071-B084-BBBA13C0F408.jpeg",
 		description: "Nuestra primera cita, me dijiste que te gustaban los nachos y desde entonces siempre los pido cuando salimos"
 	},
 	{
-		image: "./assets/img/6E051B9D-C41D-4CA7-AC20-FB86C427F065.jpeg",
+		image: "/gordi/assets/img/6E051B9D-C41D-4CA7-AC20-FB86C427F065.jpeg",
 		description: "Una de nuestras primeras fotos juntos, siempre te he dicho que adoro esa foto"
 	},
 	{
-		image: "./assets/img/7421a4a1-e11a-45c2-9370-b1f6362ab4ba.JPG",
+		image: "/gordi/assets/img/7421a4a1-e11a-45c2-9370-b1f6362ab4ba.JPG",
 		description: "Simplemente historia"
 	},
 	{
-		image: "./assets/img/a0e3bc16-4ec4-4022-80ab-3e1f0fdcdd07.jpeg",
+		image: "/gordi/assets/img/a0e3bc16-4ec4-4022-80ab-3e1f0fdcdd07.jpeg",
 		description: "La primera vez que fui a casa de erika y se pusieron a echarnos fotos en el espejo"
 	},
 	{
-		image: "./assets/img/C6BDBC65-5636-43C0-BE84-B38DFF4FC6AE.jpeg",
+		image: "/gordi/assets/img/C6BDBC65-5636-43C0-BE84-B38DFF4FC6AE.jpeg",
 		description: "Siempre te ponias mis llaves en el cuello y me encantaba"
 	},
 	{
-		image: "./assets/img/CE5F3AA5-BC0E-4A27-BB01-549F7F9188A4.jpeg",
+		image: "/gordi/assets/img/CE5F3AA5-BC0E-4A27-BB01-549F7F9188A4.jpeg",
 		description: "Ese dia ibamos por la calle larga de las portadas con una cancion de Feid y cantandola juntos"
 	},
 	{
-		image: "./assets/img/classicu 2023-08-27 214231.jpeg",
+		image: "/gordi/assets/img/classicu 2023-08-27 214231.jpeg",
 		description: "Esta foto me encanta"
 	},
 	{
-		image: "./assets/img/classicu 2024-07-25 175928.jpeg",
+		image: "/gordi/assets/img/classicu 2024-07-25 175928.jpeg",
 		description: "No no es que literalmente somos los mas wenorros"
 	},
 	{
-		image: "./assets/img/classicu 2025-07-14 124943.854.jpg",
+		image: "/gordi/assets/img/classicu 2025-07-14 124943.854.jpg",
 		description: "La foto que tengo de fondo de pantalla, creo que es de mis fotos favoritas"
 	},
 	{
-		image: "./assets/img/DC739CEE-AE16-4C8B-9972-A83F371A08BD.jpeg",
+		image: "/gordi/assets/img/DC739CEE-AE16-4C8B-9972-A83F371A08BD.jpeg",
 		description: "Este dia fue cuando fuimos al pensador a hablar de nosotros y me pediste salir"
 	},
 	{
-		image: "./assets/img/dclassic 2022-09-01 001153.jpeg",
+		image: "/gordi/assets/img/dclassic 2022-09-01 001153.jpeg",
 		description: "De las primeras veces que fuimos al pensador despues de estar saliendo oficialmente me dijiste que te gustaba golden de harry y la subi a la historia"
 	},
 	{
-		image: "./assets/img/IMG_0767.JPG",
+		image: "/gordi/assets/img/IMG_0767.JPG",
 		description: "Me encanto estar contigo en Almeria"
 	},
 	{
-		image: "./assets/img/IMG_0800.JPG",
+		image: "/gordi/assets/img/IMG_0800.JPG",
 		description: "Que guapa coño"
 	},
 	{
-		image: "./assets/img/IMG_0883.JPG",
+		image: "/gordi/assets/img/IMG_0883.JPG",
 		description: "En verdad somos tela de potentes"
 	},
 	{
-		image: "./assets/img/IMG_1176.jpg",
+		image: "/gordi/assets/img/IMG_1176.jpg",
 		description: "La primera vez que nos disfrazamos juntos y el dia que te pedi salir"
 	},
 	{
-		image: "./assets/img/IMG_1492.jpeg",
+		image: "/gordi/assets/img/IMG_1492.jpeg",
 		description: "El dia que fuimos a Sevilla con Samara y Maria, estaba super feliz porque te estaba viendo editar una foto nuestra y me hacia mucha ilusion"
 	},
 	{
-		image: "./assets/img/IMG_20250109_232545.jpg",
+		image: "/gordi/assets/img/IMG_20250109_232545.jpg",
 		description: "Me encanta que me cogas el movil y te pongas a echarnos fotos por la cara"
 	},
 	{
-		image: "./assets/img/IMG_20250112_234433 (1).jpg",
+		image: "/gordi/assets/img/IMG_20250112_234433 (1).jpg",
 		description: "Es que es nuestro hijo"
 	},
 	{
-		image: "./assets/img/IMG_3477.jpeg",
+		image: "/gordi/assets/img/IMG_3477.jpeg",
 		description: "Una de las 500 millones de veces que he estado en tu casa pero una de las primeras"
 	},
 	{
-		image: "./assets/img/IMG_4101.jpg",
+		image: "/gordi/assets/img/IMG_4101.jpg",
 		description: "De las primeras veces que fuimos a Punta Umbria juntos"
 	},
 	{
-		image: "./assets/img/IMG_4430.jpg",
+		image: "/gordi/assets/img/IMG_4430.jpg",
 		description: "El dia que nos vimos cuando llegue de Marruecos, habia dormido un total de 1 hora pero no me aguantaba sin verte"
 	},
 	{
-		image: "./assets/img/IMG_4862.jpeg",
+		image: "/gordi/assets/img/IMG_4862.jpeg",
 		description: "De las primeras veces que quede contigo y con las niñas, fuimos al bk y hacia tan buena puesta de sol que me puse a echarte fotos"
 	},
 	{
-		image: "./assets/img/IMG_7811.jpg",
+		image: "/gordi/assets/img/IMG_7811.jpg",
 		description: "Ir a ver dellafuente contigo fue una de las mejores experiencias de mi vida"
 	},
 	{
-		image: "./assets/img/IMG-20240724-WA0000.jpg",
+		image: "/gordi/assets/img/IMG-20240724-WA0000.jpg",
 		description: "El dia que me acompañaste al medico en Chiclana porque estaba apunto del fallecimiento"
 	},
 	{
-		image: "./assets/img/IMG-20240802-WA0016.jpg",
+		image: "/gordi/assets/img/IMG-20240802-WA0016.jpg",
 		description: "CHIPIIIIIIIII"
 	},
 	{
-		image: "./assets/img/IMG-20241101-WA0013.jpg",
+		image: "/gordi/assets/img/IMG-20241101-WA0013.jpg",
 		description: "Yo creo que literalmente es nuestra serie"
 	},
 	{
-		image: "./assets/img/IMG-20250119-WA0021.jpg",
+		image: "/gordi/assets/img/IMG-20250119-WA0021.jpg",
 		description: "El dia que fuimos a ver al fokin C. Tangana"
 	},
 	{
-		image: "./assets/img/IMG-20250507-WA0023.jpg",
+		image: "/gordi/assets/img/IMG-20250507-WA0023.jpg",
 		description: "Una feria mas siendo los mas wenorros"
 	},
 	{
-		image: "./assets/img/IMG-20250518-WA0049.jpg",
+		image: "/gordi/assets/img/IMG-20250518-WA0049.jpg",
 		description: "Es que en verdad eres basto de guapa jode"
 	},
 	{
-		image: "./assets/img/IMG-20250621-WA0045.jpg",
+		image: "/gordi/assets/img/IMG-20250621-WA0045.jpg",
 		description: "Un poquito mas de Madrid"
 	}
 ]; 
